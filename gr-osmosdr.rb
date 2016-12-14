605bea428205f39aafc7a77bf6258eb1b8fb3153
kolen <incredible.angst@gmail.com>
2016-12-14 15:48:12 +0300

    Add python import test

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -40,15 +40,6 @@ class GrOsmosdr < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test gr`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "python", "-c", "import osmosdr"
   end
 end
